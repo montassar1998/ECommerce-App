b009d02f381d0421bb0d7d6c02795774830b860e
Rostom Mathlouthi <mathlouthi.rostom@gmail.com>
2022-08-07 00:59:43 +0100

    Navbar, React Router, Fetch to Home, Backend Auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,32 +10,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { StoreProvider } from './Store';
-
-ReactDOM.render(
-    <React.StrictMode>
-        <StoreProvider>
-            <HelmetProvider>
-                <PayPalScriptProvider deferLoading={true}>
-                    <App />
-                </PayPalScriptProvider>
-            </HelmetProvider>
-        </StoreProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 657df4c03c4575c60508dca7cea1169c00c8a8f8
